95dabdf961d87d5b72d2bdba191ecb51164c50bb
HollyGunning <46715533+HollyGunning@users.noreply.github.com>
2020-08-04 16:15:56 +0100

    Function to set the registered user to a patient

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,39 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin') // Require the admin package from firebase
+admin.initializeApp() //Initialise admin server side
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const db = admin.firestore()
+
+exports.AddUserRole = functions.auth.user().onCreate(async (user) => {
+    // Access the user and returns a promise so a value can be returned to the user
+    return admin.auth().getUserByEmail(user.email).then(user => {
+        // With the user selected, set a custom claim for admin status
+        const customClaims = {
+            patient: true
+        }
+        try{
+            admin.auth().setCustomUserClaims(user.uid, customClaims)
+
+           return db.collection("roles").doc(user.uid).set({
+            email: user.email,
+            role: customClaims
+            
+            }).catch(error => {
+                console.log("Custom claim error is ", error)
+            })
+        } 
+        finally{
+            console.log("All Done")
+        }
+        // return admin.auth().setCustomUserClaims(user.uid, customClaims, {
+        //     admin: true,
+        //     // role: customClaims
+        // });
+    // }).then(() => {
+        // const customClaims = {
+        //     admin: true,
+        // }
+
+
+    })
+})
