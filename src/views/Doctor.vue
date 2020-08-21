2d809906a17913f8205bb27833648c8c0810aada
HollyGunning <46715533+HollyGunning@users.noreply.github.com>
2020-08-21 15:42:49 +0100

    Still working on the medical record between patient and doctor

--- src/views/Doctor.vue
+++ src/views/Doctor.vue
@@ -1,28 +1,134 @@
 <template>
 <v-container>
 <DoctorNavbar />
+  <v-col cols="12" s="12" sm="12" md="12" lg="12">
+    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <template v-slot:activator="{ on, attrs }">
+        <!-- Card for Patient Records Search Bar -->
+        <v-card outlined>
+          <v-card-title class="text-uppercase grey--text text--darken-1">Patients
+            <v-spacer></v-spacer>
+            <v-text-field
+            v-model="search"
+            append-icon="fa-search"
+            label="Search"
+            single-line
+            hide-details
+            ></v-text-field>
+          </v-card-title>
+        </v-card>
+        <!-- Card for the Data Table -->
+        <v-card outlined>
+          <v-data-table
+          :headers="headers"
+          :items="patients"
+          :search="search"
+          :items-per-page="5"
+          class="elevation-1"
+          >
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon
+              small
+              class="mr-2"
+              :key="item.id"
+              
+              v-bind="attrs"
+              v-on="on"
+            >
+              mdi-pencil
+            </v-icon>
+          </template>
+          </v-data-table>
+        </v-card>
 
- 
+      </template>
+      <!-- Dialog opens this card where the doctor can update the patients medical record-->
+      <v-card>
+        <v-card-title class="primary white--text">Update Medical Record
+          <v-spacer></v-spacer>
+          <v-btn class="mr-4" icon dark @click="dialog = false"> 
+          <v-icon class="mx-2" fab dark color="white--text darken-1 ">fa-window-close</v-icon>
+          <span>Cancel</span>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          Update Medical Record
+        </v-card-text>
+      </v-card>
+    </v-dialog>
 
-
+  </v-col>
 </v-container>
-
-
-
-
 </template>
 
 <script>
 import DoctorNavbar from '../components/Navbars/DoctorNavbar'
-
+import { auth, db } from '../firebase'
 
 export default {
-components: {
-   DoctorNavbar,
-},
+  components: {
+    DoctorNavbar,
+  },
+  created () {
+    var user = auth.currentUser
+      if(user){
+        this.currentUser = auth.currentUser.uid // Get current users ID
 
-  data: () => ({
-    //
-  }),
+        db.collection("roles").get().then(snap => {
+        snap.forEach(doc => {
+          let user = doc.data()
+          // if user is not doctor or admin retrieve the record
+          if (!user.role.admin && !user.role.doctor) {
+            // Then set user.id to the doc.id to refer back to their record in users
+            user.id = doc.id
+            db.collection("users").doc(user.id).onSnapshot(doc => {
+                let patientRecord = doc.data()
+                patientRecord.id = doc.id
+                
+                let patient = {
+                  id: patientRecord.id,
+                  name: patientRecord.firstname + ' ' + patientRecord.surname,
+                  dob: patientRecord.date,
+                  gender: patientRecord.gender,
+                  ppsn: patientRecord.ppsn
+                }
+                this.patients.push(patient)
+            })
+          }
+        })
+        })
+        
+      }else{
+        console.log("User has logged out")
+      }
+    
+
+
+  },
+  computed: {
+
+  },
+  data() {
+    return {
+      currentUser: null,
+      dialog: false,
+
+      patients: [],
+      patient: null,
+      search: '',
+
+      headers: [
+        // { text: 'Patient ID', align: 'start',  sortable: false, value: 'id' },
+        { text: 'Patient Name', value: 'name' },
+        { text: 'DoB', value: 'dob'},
+        { text: 'Gender', value: 'gender' },
+        { text: 'PPSN', value: 'ppsn' },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+
+     
+    
+    }
+  },
 };
 </script>
--- src/views/Medication.vue
+++ src/views/Medication.vue
@@ -17,6 +17,123 @@
       </v-card-title>
 
 
+      <v-card-text class="mt-4">
+  
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-autocomplete
+            v-model="medicationName"
+            :items="medicationList"
+            label="Medication Name"
+            outlined
+ 
+        ></v-autocomplete>
+        </v-col>
+        <v-col cols="6" md="3">
+          <v-text-field 
+          label="Dosage" 
+          type="number" 
+          outlined
+          ></v-text-field>
+        </v-col>
+        <v-col cols="6" md="3">
+          <v-select 
+          v-model="dosageUnit"
+          :items="units"
+          label="Units"
+          ></v-select>
+        </v-col>
+        <v-col cols="6" md="6">
+          <v-textarea 
+          auto-grow
+          rows="1"
+          v-model="additionalDetails"
+          label="Additional Details"
+          :counter="150"
+          outlined
+          ></v-textarea>
+        </v-col >
+
+
+      
+        <v-col cols="12" md="4">
+        <v-menu
+        v-model="menu2"
+        :close-on-content-click="false"
+        max-width="290"
+        >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+          label="Start Date"
+          readonly
+          :value="formattedDate"
+          v-bind="attrs"
+          v-on="on"
+          required
+          outlined    
+          ></v-text-field>
+        </template>
+        <v-date-picker 
+        full-width
+        scrollable
+        show-current
+        v-model="medicationStartDate" 
+        @input="menu2 = false"
+        @change="onDateChanged()"
+        >
+        </v-date-picker>
+        </v-menu>
+        </v-col>
+
+
+
+        <!-- SCHEDULE SELECTOR -->
+        <v-col cols="12" md="4">
+            <v-select
+            label="Schedule"
+            :items="schedules"
+            item-text="text"
+            item-value="value"
+            @change="onScheduleChange($event)"
+            outlined
+            ></v-select>
+        </v-col>
+
+  
+      <!-- Select days of the week -->
+     <v-col cols="12" md="8">
+        <v-card flat v-if="showEveryXDays">
+          <span class="overline">Specify Days of the Week</span>
+          <v-chip-group 
+          column 
+          v-model="selectedDays"
+          mandatory 
+          multiple 
+          active-class="primary--text"
+          @change="onDaysChange()"
+          >
+            <v-chip v-for="(day, value) in daysofWeek" :key="value" >
+              {{ day.text }}
+            </v-chip>
+          </v-chip-group>
+        </v-card>
+      </v-col>
+ 
+    <v-col>
+      <v-card flat v-if="showEveryXDays">
+        <span class="overline">How many times a day</span>
+        
+      </v-card>
+    </v-col>
+
+    
+
+
+
+      </v-row>
+
+      </v-card-text>
+
 
       </v-card>
 
@@ -36,14 +153,78 @@ export default {
   },
   data() {
     return {
+      menu2: false,
+      medicationStartDate: null,
+      formattedDate: null,
       
+      dosageUnit: null,
+      units: [
+        { text: 'Tablet(s)', value: 'tablet' },
+        { text: 'Capsule(s)', value: 'capsule' },
+        { text: 'Injection(s)', value: 'injection' },
+        { text: 'Drop(s)', value: 'drop' },
+        { text: 'Spray(s)', value: 'spray' },
+        { text: 'Suppository(ies)', value: 'suppository' },
+        { text: 'Tablespoon(s)', value: 'tbs' },
+        { text: 'Teaspoon(s)', value: 'tsp' },
+        { text: 'Milligram(s)', value: 'milligram' },
+        { text: 'Milliliter(s)', value: 'milliliter' },
+        
+        
+      ],
+      medicationName: null,
+      medicationList: [],
+
+
+
+      additionalDetails: null,
+
+
+      schedules: [
+        { text: 'Daily', value: 'daily' },
+        { text: 'Every X days', value: 'everyXdays' },
+        { text: 'Specific Dates', value: 'specific' },
+      ],
+      showEveryXDays: false,     
+      daysofWeek: [ 
+        { text: 'Monday', value: 'monday'},
+        { text: 'Tuesday', value: 'tuesday'},
+        { text: 'Wednesday', value: 'wednesday'},
+        { text: 'Thursday', value: 'thursday'},
+        { text: 'Friday', value: 'friday'},
+        { text: 'Saturday', value: 'saturday'},
+        { text: 'Sunday', value: 'sunday'},
+      ],
+      selectedDays: [],
+    
     }
   },
   created() {
     
   },
   methods: {
-    
+    onScheduleChange (value) {
+
+      if(value == 'daily' ){
+        this.showEveryXDays = false
+        console.log("Daily Here")
+      }
+      else if (value == 'everyXdays'){
+        this.showEveryXDays = true
+        console.log("Every X Days Here")
+      }
+      else if(value == 'specific'){
+        this.showEveryXDays = false
+      }
+      else{
+        this.showEveryXDays = false
+      }
+      
+    },
+
+    onDaysChange () {
+      console.log(this.selectedDays)
+    }
   },
 };
 </script>
