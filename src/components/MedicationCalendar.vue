2b08f1058e47680ae46cc86bd83114a30cd237ca
HollyGunning <46715533+HollyGunning@users.noreply.github.com>
2020-08-24 16:09:13 +0100

    Attempting to input the calendar to add and track medicine

---
+++ src/components/MedicationCalendar.vue
@@ -0,0 +1,487 @@
+<template>
+    <v-row class="fill-height">
+        <v-col>
+
+        <!-- Contains the top toolbar with the today, prev, next btn options and a menu that switches the calendar view--> 
+        <v-sheet height="64">
+            <v-toolbar flat color="white">
+                <v-btn outlined color="primary" class="mr-2" @click="dialog = true">
+                    Add Medication
+                </v-btn>
+                <v-btn outlined color="primary" @click="setToday">Today</v-btn>
+                <v-btn fab text small color="primary" @click="prev">
+                    <v-icon small >mdi-chevron-left</v-icon>
+                </v-btn>
+                <v-btn fab text small color="primary" @click="next">
+                    <v-icon small>mdi-chevron-right</v-icon>
+                </v-btn>
+                <v-toolbar-title v-if="$refs.calendar">{{ $refs.calendar.title  }}</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-menu bottom right>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn 
+                        outlined
+                        color="primary"
+                        v-bind="attrs"
+                        v-on="on"
+                        >
+                        <span>{{ typeToLabel[type] }}</span>
+                        <v-icon right>mdi-menu-down</v-icon>
+                        </v-btn>
+                    </template>
+                        <v-list>
+                            <v-list-item @click="type = 'day'">
+                                <v-list-item-title>Day</v-list-item-title>
+                            </v-list-item>
+                            <v-list-item @click="type = 'week'">
+                                <v-list-item-title>Week</v-list-item-title>
+                            </v-list-item>
+                            <v-list-item @click="type = 'month'">
+                                <v-list-item-title>Month</v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                </v-menu>
+            </v-toolbar>
+        </v-sheet>
+
+        <!-- Dialog for adding medication -->
+        <v-dialog v-model="dialog" persistent max-width="600px">
+        <v-card>
+            <v-form @submit.prevent="saveMedication()">
+            <v-card-title class="primary lighten-1 white--text">Add Medication
+                <v-spacer></v-spacer>
+                <v-btn class="mr-6" icon dark @click="dialog = false"> 
+                <v-icon class="mx-2" fab dark color="white--text darken-1 ">fa-window-close</v-icon>
+                <span>Cancel</span>
+                </v-btn>
+            </v-card-title>
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <!-- Name -->
+                        <v-col cols="12" sm="5" md="5">
+                            <v-text-field
+                            label="Medication Name*"
+                            v-model="medicationName" 
+                            type="text" 
+                            outlined
+
+
+
+                            ></v-text-field>
+                        </v-col>
+                        <!-- Dose Amount -->
+                        <v-col cols="12" sm="3" md="3">
+                            <v-text-field
+                            label="Dose*"
+                            type="number"
+                            v-model="dose"
+                            outlined
+
+
+
+                            ></v-text-field>
+                        </v-col>
+                         <!-- Select Type -->
+                        <v-col cols="12" sm="4" md="4">
+                            <v-select
+                            label="Unit*"
+                            v-model="dosageUnit"
+                            :items="units"
+                            outlined
+
+
+
+                            ></v-select>
+                        </v-col>
+                        <!-- Additional Details --> 
+                        <v-col cols="12" sm="6" md="6">
+                             <v-textarea 
+                             label="Additional Detail"
+                             v-model="medicationDetails" 
+                             auto-grow
+                             rows="1"
+                             outlined 
+                             
+
+
+                             ></v-textarea>
+                        </v-col>
+                        <!-- Select Time -->
+                        <v-col cols="12" sm="6" md="6">
+                        <v-menu
+                            ref="menu"
+                            v-model="menu"
+                            :close-on-content-click="false"
+                            :nudge-right="40"
+                            :return-value.sync="time"
+                            transition="scale-transition"
+                            offset-y
+                            max-width="290px"
+                            min-width="290px"
+                        >
+                            <template v-slot:activator="{ on, attrs }">
+                            <v-text-field
+                                v-model="time"
+                                label="Select Time*"
+                                prepend-icon="access_time"
+                                readonly
+                                v-bind="attrs"
+                                v-on="on"
+                                outlined
+                            ></v-text-field>
+                            </template>
+                            <v-time-picker
+                            v-if="menu"
+                            v-model="time"
+                            full-width
+                            @click:minute="$refs.menu.save(time)"
+                            ></v-time-picker>
+                        </v-menu>
+                        </v-col>
+
+
+                        <v-col cols="12" sm="6" md="6">
+                            <v-select   
+                            label="Date Options"
+                            v-model="dateOption"
+                            :items="chooseDateOptions"
+                            outlined
+                            @change="setOption(dateOption)"
+
+
+                            ></v-select>
+                        </v-col>
+                        
+                         <!-- Multiple Dates Picker -->
+                        <v-col cols="12" sm="6" md="6" v-if="showMultiple">
+                        <v-menu
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        max-width="290"
+                        >
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                        label="Select Days*"
+                        readonly
+                        :value="medDates"
+                        v-bind="attrs"
+                        v-on="on"
+                        required
+                        outlined
+                        ></v-text-field>
+                        </template>
+                        <v-date-picker 
+                        full-width
+                        scrollable
+                        show-current
+                        :min="getTodaysDate()"
+                        :max="getFutureDate()"
+                        v-model="medDates"
+                        multiple
+                        >
+                        </v-date-picker>
+                        </v-menu>
+                        </v-col>
+
+
+                    <!-- Ranged Dates Picker -->
+                        <v-col cols="12" sm="6" md="6" v-if="showRange">
+                        <v-menu
+                        v-model="menu3"
+                        :close-on-content-click="false"
+                        max-width="290"
+                        >
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                        label="Select Date Range*"
+                        readonly
+                        :value="medBetweenDates"
+                        v-bind="attrs"
+                        v-on="on"
+                        required
+                        outlined
+                        ></v-text-field>
+                        </template>
+                        <v-date-picker 
+                        full-width
+                        scrollable
+                        show-current
+                        :min="getTodaysDate()"
+                        :max="getFutureDate()"
+                        v-model="medBetweenDates"
+                        range
+                        >
+                        </v-date-picker>
+                        </v-menu>
+                        </v-col>
+
+
+                      <!-- Select a Colour --> 
+                        <v-col cols="12" sm="6" md="6">
+                       <v-menu
+                        v-model="menu3"
+                        :close-on-content-click="false"
+                        max-width="290"
+                        >
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                        label="Select Colour*"
+                        readonly
+                        :value="picker"
+                        v-bind="attrs"
+                        v-on="on"
+                        required
+                        outlined
+                        ></v-text-field>
+                        </template>
+                        <v-color-picker
+                            v-model="picker"
+                            flat
+                        ></v-color-picker>
+                        </v-menu>
+                        </v-col>
+
+
+
+                    </v-row>
+                </v-container>
+            </v-card-text>
+            <v-card-actions>
+                <v-col cols="12" class="mt-n6">
+                    <v-btn
+                    block class="primary white--text"
+                    @click.prevent="saveMedication()">
+                    <span>Add Medication</span>
+                    </v-btn>
+                </v-col>  
+            </v-card-actions>
+        </v-form>
+        </v-card>
+        </v-dialog>
+
+        <v-sheet height="500">
+            <v-calendar
+            ref="calendar"
+            color="primary"
+            :type="type"
+            v-model="focus"
+            :events="events"
+            @click:date="viewDay"
+            @click:event="showEvent"
+            >
+            </v-calendar>
+                <v-menu
+                v-model="selectedOpen"
+                :close-on-content-click="false"
+                :activator="selectedElement"
+                offset-x
+                >
+                <v-card
+                color="grey lighten-4"
+                min-width="350px"
+                flat
+                >
+                <v-toolbar class="primary" :color="selectedEvent.color" dark>
+                <v-toolbar-title class="primary white--text" v-html="selectedEvent.name"></v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn
+                text
+                color="white"
+                @click="selectedOpen = false"
+                >
+                Cancel
+                </v-btn>
+                </v-toolbar>
+                <v-card-text>
+                <span v-html="selectedEvent.details"></span>
+                </v-card-text>
+                </v-card>
+                </v-menu>
+
+        </v-sheet>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import { auth, db } from '../firebase'
+
+export default {
+    data() {
+        return {
+            focus: '',
+            type: 'month',
+            typeToLabel: {
+                month: 'Month',
+                week: 'Week',
+                day: 'Day',
+            },
+
+            selectedEvent: {},
+            selectedElement: null,
+            selectedOpen: false,
+            events: [],
+
+            dialog: false, // add medication
+            menu: false, // time picker
+            medicationName: null,
+            dose: null,
+            dosageUnit: null,
+            units: [
+                { text: 'Tablet(s)', value: 'tablet' },
+                { text: 'Capsule(s)', value: 'capsule' },
+                { text: 'Injection(s)', value: 'injection' },
+                { text: 'Drop(s)', value: 'drop' },
+                { text: 'Spray(s)', value: 'spray' },
+                { text: 'Suppository(ies)', value: 'suppository' },
+                { text: 'Tablespoon(s)', value: 'tbs' },
+                { text: 'Teaspoon(s)', value: 'tsp' },
+                { text: 'Milligram(s)', value: 'milligram' },
+                { text: 'Milliliter(s)', value: 'milliliter' },
+            ],
+            medicationDetails: null,
+            time: null,
+            
+            dateOption: null,
+            chooseDateOptions: [
+                { text: 'Single/Multiple Day(s)', value: 'multiple'},
+                { text: 'Date Range', value: 'range'},
+            ],
+
+            showMultiple: false, 
+            menu2: null, // multiples picker
+            medDates: [],
+
+            showRange: false,
+            menu3: false, // range picker
+            medBetweenDates: [],
+
+            menu4: false,
+            picker: null,
+            
+          
+            
+        }
+    },
+    mounted() {
+        this.getAppointments()
+    },
+    methods: {
+        getTodaysDate () {
+            let today = new Date ()
+            today.setDate(today.getDate())
+            return today.toISOString()
+        },
+        getFutureDate () {
+            let latest = new Date ()
+            latest.setMonth(latest.getMonth() + 1)
+            return latest.toISOString()
+        },
+        getAppointments () {
+            // Set current user to the currently logged in user
+            var currentUser = auth.currentUser.uid
+            db.collection("appointments").where("patientID", "==", currentUser).onSnapshot( snap => {
+                let appointment = snap.docChanges()
+                appointment.forEach(appointment => { 
+                    let record = appointment.doc.data()
+                
+                    let time = record.appointmentTime.replace(".", ":")
+                    let endTime = time.substr(0, time.length -2)
+                    let endTimeMinutes = time.substr(time.length -2, 2)
+                    endTime = endTime + (parseInt(endTimeMinutes) + 25)
+
+                    let event = {
+                        name: 'Appointment',
+                        details : record.appointmentDetails,
+                        start: record.appointmentDate + " " + time,
+                        end: record.appointmentDate + " " + endTime
+                    }
+                    this.events.push(event)
+                  
+                
+                })
+            })
+        },
+        setOption () {
+            // Switch between the two date picking options
+            if(this.dateOption == 'multiple'){
+                this.medBetweenDates = []
+                this.showRange = false
+                this.showMultiple = true
+            }
+            else{
+                this.medDates = []
+                this.showMultiple = false
+                this.showRange = true
+            }
+        },
+        saveMedication () {
+      
+            // Check can these be added in optionally for display on card
+            // let dose = this.dose
+            // let unit = this.dosageUnit
+
+            // // These will need to be configured to start and end
+            // let time = this.time
+            // let dateMultiple = this.medDates
+            // let dateRange = this.dateRange
+            
+
+            let medication = {
+               name: this.medicationName,
+               details: this.medicationDetails,
+               start: this.medDates + " " + this.time,
+                end: this.medDates + " " + this.time,
+                color: this.picker,
+            }
+            this.events.push(medication)
+
+            this.medicationName = null
+            this.dose = null
+            this.dosageUnit = null
+            this.medicationDetails = null
+            this.time = null
+            this.medDates= []
+            this.medBetweenDates = null
+            this.picker = null
+        },
+
+
+
+
+
+
+        viewDay ({ date }) {
+            this.focus = date
+            this.type = 'day'
+        },
+        setToday () {
+            this.focus = ''
+        },
+        prev () {
+            this.$refs.calendar.prev()
+        },
+        next () {
+            this.$refs.calendar.next()
+        },
+
+        showEvent ({ nativeEvent, event }) {
+            const open = () => {
+            this.selectedEvent = event
+            this.selectedElement = nativeEvent.target
+            setTimeout(() => this.selectedOpen = true, 10)
+            }
+
+            if (this.selectedOpen) {
+            this.selectedOpen = false
+            setTimeout(open, 10)
+            } else {
+            open()
+            }
+
+            nativeEvent.stopPropagation()
+      },
+    },
+}
+</script>
--- src/views/Medication.vue
+++ src/views/Medication.vue
@@ -1,114 +1,23 @@
 <template>
 <v-container>
-<Navbar />  
-    <v-row>
-      <v-col cols="12" s="12" sm="12" md="12" lg="12">
+<Navbar />
 
-      <v-card flat>
-      <v-card-title class="primary lighten-1 white--text">Medication
-      <v-spacer></v-spacer>
-      <v-btn
-      class="mx-2" 
-      fab small
-      dark color="primary"
-      >
-      <v-icon>mdi-plus</v-icon>
-      </v-btn>
-      </v-card-title>
-      <v-card-text>
+  <v-card id="Medication Calendar" flat>
+    <MedCalendar />
+  </v-card>
 
-      </v-card-text>
-      </v-card>
- 
-
-      </v-col></v-row>
 </v-container>
 </template>
 
 <script>
 import Navbar from '../components/Navbars/Navbar'
+import MedCalendar from '../components/MedicationCalendar'
 
 export default {
   components: {
     Navbar,
+    MedCalendar,
   },
-  computed: {
-    
-  },
-  data() {
-    return {
-      menu2: false,
-      medicationStartDate: null,
-      formattedDate: null,
-      
-      dosageUnit: null,
-      units: [
-        { text: 'Tablet(s)', value: 'tablet' },
-        { text: 'Capsule(s)', value: 'capsule' },
-        { text: 'Injection(s)', value: 'injection' },
-        { text: 'Drop(s)', value: 'drop' },
-        { text: 'Spray(s)', value: 'spray' },
-        { text: 'Suppository(ies)', value: 'suppository' },
-        { text: 'Tablespoon(s)', value: 'tbs' },
-        { text: 'Teaspoon(s)', value: 'tsp' },
-        { text: 'Milligram(s)', value: 'milligram' },
-        { text: 'Milliliter(s)', value: 'milliliter' },
-        
-        
-      ],
-      medicationName: null,
-      medicationList: [],
 
-
-
-      additionalDetails: null,
-
-
-      schedules: [
-        { text: 'Daily', value: 'daily' },
-        { text: 'Every X days', value: 'everyXdays' },
-        { text: 'Specific Dates', value: 'specific' },
-      ],
-      showEveryXDays: false,     
-      daysofWeek: [ 
-        { text: 'Monday', value: 'monday'},
-        { text: 'Tuesday', value: 'tuesday'},
-        { text: 'Wednesday', value: 'wednesday'},
-        { text: 'Thursday', value: 'thursday'},
-        { text: 'Friday', value: 'friday'},
-        { text: 'Saturday', value: 'saturday'},
-        { text: 'Sunday', value: 'sunday'},
-      ],
-      selectedDays: [],
-    
-    }
-  },
-  created() {
-    
-  },
-  methods: {
-    onScheduleChange (value) {
-
-      if(value == 'daily' ){
-        this.showEveryXDays = false
-        console.log("Daily Here")
-      }
-      else if (value == 'everyXdays'){
-        this.showEveryXDays = true
-        console.log("Every X Days Here")
-      }
-      else if(value == 'specific'){
-        this.showEveryXDays = false
-      }
-      else{
-        this.showEveryXDays = false
-      }
-      
-    },
-
-    onDaysChange () {
-      console.log(this.selectedDays)
-    }
-  },
-};
+}
 </script>
