2c310cc1fbd423418f0b8fc6908691e4fa646b80
HollyGunning <46715533+HollyGunning@users.noreply.github.com>
2020-09-02 14:35:53 +0100

    Code is complete and has been commented

--- src/views/MedicalRecord.vue
+++ src/views/MedicalRecord.vue
@@ -1,13 +1,13 @@
 <template>
 <v-container>
-<Navbar />
+<Navbar /> <!-- Import Patient Nav toolbar & drawer -->
 <v-row><v-col cols="12" md="12" lg="12">
   <v-card flat>
     <v-card-title class="primary lighten-1 white--text">Medical Record
     <v-spacer></v-spacer>
     </v-card-title>
       <v-row>  
-      <!-- GENDER -->
+      <!-- Gender Field -->
       <v-col cols="12" sm="6" md="6" lg="3">
        <v-card flat>
           <v-card-title class="overline">
@@ -21,8 +21,8 @@
             color="primary" 
             group 
             mandatory
-            @change="onGenderChange(gender)"
-            >
+            @change="onGenderChange(gender)" 
+            > <!-- onGenderChange method method carries over selected value -->
               <v-btn depressed x-large color="primary--text darken-1" value="Female">
               <v-icon class="mr-2">fa-venus</v-icon>Female
               </v-btn>
@@ -33,7 +33,7 @@
           </v-card-text>
       </v-card>
       </v-col>
-      <!-- BLOODS -->
+      <!-- Blood Type Field -->
       <v-col cols="12" sm="6" md="6" lg="3">
        <v-card flat>
           <v-card-title class="overline">
@@ -49,12 +49,12 @@
                 prepend-icon="fa-syringe"
                 outlined
                 @change="onBloodsChange(selectedBlood)"
-              ></v-select>
+              ></v-select> <!-- onBloodsChange method carries over selected value -->
           </v-card-text>
       </v-card>
       </v-col>
 
-      <!-- WEIGHT AND HEIGHT -->
+      <!-- Weight & Height Field -->
       <v-col cols="12" sm="6" md="6" lg="3">
       <v-card flat>
         <v-card-title>                   
@@ -74,7 +74,7 @@
           :error-messages="weightError"
           @input="$v.weight.$touch(weight)"
           @blur="$v.weight.$touch(weight)"
-          ></v-text-field>
+          ></v-text-field> <!-- onWeightChange method carries over inputted value -->
         </v-card-text>
       </v-card> 
       </v-col>
@@ -97,11 +97,11 @@
               :error-messages="heightError"
               @input="$v.height.$touch(height)"
               @blur="$v.height.$touch(height)"
-              ></v-text-field>
+              ></v-text-field> <!-- onHeightChange method carries over inputted value -->
           </v-card-text>
         </v-card>
       </v-col>
-      <!-- Pulse -->
+      <!-- Pulse Field (readonly) -->
       <v-col cols="12" sm="4" md="4" lg="3">
       <v-card flat>
         <v-card-title>
@@ -121,7 +121,7 @@
         </v-card-text>
       </v-card>
       </v-col>
-      <!-- Blood Pressure & Pulse -->
+      <!-- Blood Pressure & Pulse Fields (readonly) -->
       <v-col cols="12" sm="4" md="4" lg="3">
       <v-card flat>
         <v-card-title>
@@ -156,7 +156,7 @@
         </v-card-text>
       </v-card>
       </v-col>
-      <!-- Blood Glucose Level -->
+      <!-- Blood Glucose Level Field (readonly) -->
       <v-col cols="12" sm="4" md="4" lg="3">
       <v-card flat>
         <v-card-title>
@@ -174,7 +174,7 @@
         </v-card-text>
       </v-card>
       </v-col>
-      <!-- Cholesterol -->
+      <!-- Cholesterol Fields (readonly) -->
       <v-col cols="12" sm="6" md="6" lg="3">
       <v-card flat>
         <v-card-title>
@@ -192,7 +192,6 @@
         </v-card-text>
       </v-card>
       </v-col>
-
       <v-col cols="12" sm="6" md="6" lg="3">
       <v-card flat>
         <v-card-title>
@@ -211,7 +210,6 @@
         </v-card-text>
       </v-card>
       </v-col>
-
       <v-col cols="12" sm="6" md="6" lg="3">
       <v-card flat>
         <v-card-title>
@@ -230,7 +228,6 @@
         </v-card-text>
       </v-card>
       </v-col>
-
       <v-col cols="12" sm="6" md="6" lg="3">
       <v-card flat>
         <v-card-title>
@@ -249,8 +246,7 @@
         </v-card-text>
       </v-card>
       </v-col>
-
-
+    <!-- Tabs for Allergies, Conditions & Immunisations -->
     <v-col cols="12">
       <v-tabs v-model="tab">
         <v-tab v-for="tab in tabs" :key="tab.tabName">{{ tab.tabName }}</v-tab>
@@ -259,19 +255,17 @@
       <v-tabs-items v-model="tab">
         <v-tab-item> <!-- ALLERGIES -->
           <v-card >
-              <!-- Import Component -->
-              <Allergies />
+              <Allergies /> <!-- Import Allergies Component -->
                 <v-col cols="12" md="12">
+                <!-- For every allergy in the allergies array, recognised seperately by index position -->
                 <v-card outlined class="mt-2" v-for="(allergy, index) in allergies" :key="index">
                   <v-card-title class="primary lighten-1 white--text">
                     <v-icon class="mr-4 white--text">fa-allergies</v-icon>
                     {{ allergy.allergyName }}
-                  
                   <v-subheader class="overline primary lighten-1 white--text">
                   {{ allergy.allergyType }}
                   </v-subheader>
-                    <v-spacer></v-spacer>
-                    <!--  DELETE  ALLERGY -->
+                    <v-spacer></v-spacer> <!-- deleteAllergy method called on click of icon -->
                     <v-icon right @click="deleteAllergy(allergy)">fa-trash</v-icon>
                   </v-card-title>
                     <v-card-text>
@@ -307,16 +301,15 @@
         </v-tab-item>
         <v-tab-item> <!-- CONDITIONS -->
           <v-card>
-            <!-- Import Component -->
-            <Conditions />
+            <Conditions /> <!-- Import Conditions Component -->
                <v-col cols="12" md="12">
+                <!-- For every condition in the conditions array, recognised seperately by index position -->
                 <v-card outlined class="mt-2" v-for="(condition, index) in conditions" :key="index">
                   <v-card-title class="primary lighten-1 white--text">
                     <v-icon class="mr-4 white--text">fa-file-medical-alt</v-icon>
                     {{ condition.conditionName }}
                     <v-subheader class="overline primary lighten-1 white--text"></v-subheader>
-                    <v-spacer></v-spacer>
-                    <!--  DELETE  CONDITION -->
+                    <v-spacer></v-spacer> <!-- deleteCondition method called on click of icon -->
                     <v-icon right @click="deleteCondition(condition)">fa-trash</v-icon>
                   </v-card-title>
                     <v-card-text>
@@ -353,15 +346,15 @@
         </v-tab-item>
         <v-tab-item><!-- immunisations --> 
           <v-card>
-            <immunisations />
+            <immunisations /> <!-- Import Immunisations Component -->
               <v-col cols="12" md="12">
+              <!-- For every immunisation in the immunisations array, recognised seperately by index position -->
               <v-card outlined class="mt-2" v-for="(immunisation, index) in immunisations" :key="index">
                 <v-card-title class="primary lighten-1 white--text">
                   <v-icon class="mr-4 white--text">fa-syringe</v-icon>
                   {{ immunisation.immunisationName }}
                   <v-subheader class="overline primary lighten-1 white--text"></v-subheader>
-                  <v-spacer></v-spacer>
-                  <!--  DELETE  IMMUNISATION -->
+                  <v-spacer></v-spacer> <!-- deleteImmunisation method called on click of icon -->
                     <v-icon right @click="deleteImmunisation(immunisation)">fa-trash</v-icon>
                 </v-card-title>
                   <v-card-text>
@@ -408,22 +401,20 @@
               </v-col>
           </v-card>
         </v-tab-item>
-
-
       </v-tabs-items>
     </v-col>
-       <v-snackbar
-        :color="color"
-        v-model="snackbar"
-        :timeout="timeout"
-        :multi-line="multiLine"
-      >{{ snackbarText }}
-      </v-snackbar>
+    <!-- Snackbar to display any success or error messages -->
+    <v-snackbar
+      :color="color"
+      v-model="snackbar"
+      :timeout="timeout"
+      :multi-line="multiLine"
+    >{{ snackbarText }}
+    </v-snackbar>
     </v-row>
   </v-card>
 
 </v-col></v-row> <!-- End of display -->
-
 </v-container>
 </template>
 
@@ -443,11 +434,10 @@ export default {
   },
   created() {
     this.currentUser = auth.currentUser.uid // Get current users ID
-    // Realtime listen to the patients record in the users collection
+    // Realtime listen to the patients medical record fields in the users collection
     db.collection("users").doc(this.currentUser).onSnapshot(doc => {
-      let storedRecord = doc.data()
+      let storedRecord = doc.data() // store the doc.data() into a value to get each field
       storedRecord.id = doc.id
-    
       // Populate these values to display to the user what fields are currently holding info
       this.gender = storedRecord.gender
       this.selectedBlood = storedRecord.bloodType
@@ -461,14 +451,14 @@ export default {
       this.cholesterolLDL = storedRecord.cholesterolLDL
       this.cholesterolHDL = storedRecord.cholesterolHDL
       this.cholesterolTriglycerides = storedRecord.cholesterolTriglycerides
-      
-      // Populate the arrays with corresponding data from users record
+      // Populate arrays for allergies, conditions & immunisations
       this.allergies = storedRecord.allergy
       this.conditions = storedRecord.condition
       this.immunisations = storedRecord.immunisation
     })
   },
   computed: {
+    // Validation Errors
     weightError () {
       const errors = []
       if(!this.$v.weight.$dirty) return errors
@@ -485,12 +475,13 @@ export default {
   data() {
     return {
       currentUser: null,
+      // Snackbar fields
       snackbar: false,
       color: null,
       multiLine: true,
       timeout: 5000,
       snackbarText: "",
-      // Tabs for upcoming and past
+      // Tabs to switch between allergies, conditions & immunisations
       tab: null,
       tabs: [
       { tabName: 'Allergies' },
@@ -501,6 +492,7 @@ export default {
       weight: null, // Used to store weight
       height: null, // Used to store height
       selectedBlood: null, // Used to store the selected blood type
+      // Blood choices
       bloods: [
         {text: 'None'},
         {text: 'A Positive'},
@@ -520,27 +512,29 @@ export default {
       cholesterolLDL: null, // Used to store cholesterolLDL
       cholesterolHDL: null, // Used to store cholesterolHDL
       cholesterolTriglycerides: null, // Used to store cholesterolTriglycerides
-  
-      
+      // Arrays
       allergies: [], // Store allergies here to display to user
       conditions: [], // Store conditions here to display to user
       immunisations: [], // Store immunisations here to display to user
     }
   },
   validations: {
+    // numeric values only
     weight: { numeric },
     height: { numeric }
   },
   methods: {
-    // Used for error and success messages
+    // Error and success messages
     triggerSnackbar (message, color) {
       this.snackbarText = message,
       this.color = color,
       this.snackbar = true
     },
+    // Gender button changed
     onGenderChange (gender) {
       // Access the users collection then update gender value
       db.collection("users").doc(this.currentUser).get().then(() =>{
+        // so long as gender is not null
         if(gender != null){
           db.collection("users").doc(this.currentUser).update({
             gender: gender
@@ -548,9 +542,11 @@ export default {
         }
       })
     },
+    // Blood selection changed
     onBloodsChange(selectedBlood) {
       // Access the users collection then update blood type
       db.collection("users").doc(this.currentUser).get().then(() =>{
+        // so long as blood type is not null
         if(selectedBlood != null){
           db.collection("users").doc(this.currentUser).update({
             bloodType: selectedBlood
@@ -558,11 +554,13 @@ export default {
         }
       })
     },
+    // Weight value changed
     onWeightChange (weight) {
       // Check to see if entered data is dirty before updating the db
       this.$v.$touch()
       this.formTouched = !this.$v.$anyDirty
       this.errors = this.$v.$anyError
+      // if no errors and no dirty values
       if(this.errors === false && this.formTouched === false){
          db.collection("users").doc(this.currentUser).get().then(() =>{
         // Access the users collection then update weight if not null
@@ -574,6 +572,7 @@ export default {
       })
       }
     },
+    // Height value changed
     onHeightChange (height) {
       // Check to see if entered data is dirty before updating the db
       this.$v.$touch()
@@ -590,29 +589,31 @@ export default {
         })
       }
     },
+    // NB. fieldValue - a sentinel value that can be used to tell server to remove
+    //                  given elements from any array value that exists on the server
+    // NB. arrayRemove - removes the exact item specified from the array
 
-
+    // Delete an allergy
     deleteAllergy(allergy){
       db.collection("users").doc(this.currentUser).update({
+        // remove the allergy from the object allergies from array
         allergy: fieldValue.arrayRemove(allergy)
-      })
-      
+      }) 
     },
-
+  	// Delete a condition
     deleteCondition (condition) {
        db.collection("users").doc(this.currentUser).update({
+         // remove the condition from the object conditions from array
         condition: fieldValue.arrayRemove(condition)
       })
     },
-
+    // Delete an immunisation
     deleteImmunisation (immunisation) {
       db.collection("users").doc(this.currentUser).update({
+        // remove the immunisation from the object immunisations from array
         immunisation: fieldValue.arrayRemove(immunisation)
       })
     },
-
-
-
   },
 };
 </script>
