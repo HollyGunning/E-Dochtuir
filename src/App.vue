b26074107bbb8cb00b59958e045d64b12c087c8e
HollyGunning <46715533+HollyGunning@users.noreply.github.com>
2020-09-02 20:43:46 +0100

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
   <v-app> <!-- Route Vuetify Component use only once to wrap everything else --> 
-
-
-    <v-main class="mx-2 mb-4"> <!-- All page content will be loaded here --> 
+    <v-main class="mx-2 mb-4">
        <router-view></router-view> <!-- This will load the specific route the user is on e.g. Home, About etc. -->
     </v-main>
-
-
   </v-app>
 </template>
 
@@ -14,17 +10,6 @@
 
 export default {
   name: 'App',
-  
-  data() {
-    return {
-      
-    }
-  },
-  components: {
 
-  },
-  computed: {
-
-  }
 };
 </script>
